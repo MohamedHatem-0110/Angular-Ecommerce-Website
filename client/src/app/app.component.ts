4c26a1fe7d93c993490fe5e251f52a8d543b00e2
Mohamed Hatem <83113802+MohamedHatem-0110@users.noreply.github.com>
2024-06-18 20:25:48 +0300

    footer

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { CommonModule } from '@angular/common';
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,CommonModule,TopHeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,CommonModule,TopHeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
